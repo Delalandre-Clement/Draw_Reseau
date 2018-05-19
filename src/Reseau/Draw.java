e77a8223f205d7c05e58e426e0bcfbeb096fa490
Delalandre Clément <33206273+Delalandre-Clement@users.noreply.github.com>
2018-05-19 10:21:09 +0200

    Remise du projet au niveau de la connexion client serveur

--- src/Reseau/Draw.java
+++ src/Reseau/Draw.java
@@ -1,132 +1,34 @@
 package Reseau;
 
-import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Panel;
-import javax.swing.JToggleButton;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import javax.swing.ImageIcon;
 
-public class Draw extends JFrame {
 
-	private JFrame frame;
-	private JTextField textField;
-	private JTextArea textArea;
-	private Socket socket;
-    private BufferedReader in;
-    private PrintWriter out;
+public class Draw {
+
+
 
 	/**
-	 * Create the application.
+	 * Launch the application.
+	 * @throws IOException 
 	 */
-	public Draw() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	public void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 547, 410);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		textField = new JTextField();
-		textField.setBounds(404, 322, 127, 20);
-		textField.setColumns(10);
-
-		textArea = new JTextArea();
-		textArea.setBounds(404, 0, 127, 316);
-		textArea.setEnabled(false);
-
-		JButton btnEnvoyer = new JButton("Envoyer");
-		btnEnvoyer.setBounds(404, 348, 127, 23);
-		btnEnvoyer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				setText(textField.getText());
-			}
-		});
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(textArea);
-		frame.getContentPane().add(textField);
-		frame.getContentPane().add(btnEnvoyer);
-
-		Panel panel = new Panel();
-		panel.setBounds(10, 328, 373, 33);
-		frame.getContentPane().add(panel);
+	public static void main(String[] args) throws IOException {
+		String sentence;
+		  String modifiedSentence;
+		  BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		  Socket clientSocket = new Socket("127.0.0.1", 6789);
+		  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		  sentence = inFromUser.readLine();
+		  outToServer.writeBytes(sentence + '\n');
+		  modifiedSentence = inFromServer.readLine();
+		  System.out.println("FROM SERVER: " + modifiedSentence);
+		  clientSocket.close();
 		
-		JToggleButton tglbtnGauche = new JToggleButton("Gauche");
-		panel.add(tglbtnGauche);
-		
-		JToggleButton tglbtnDroite = new JToggleButton("Droite");
-		panel.add(tglbtnDroite);
-
-		JToggleButton tglbtnLigne = new JToggleButton("Ligne");
-		panel.add(tglbtnLigne);
-
-		JToggleButton tglbtnRectangle = new JToggleButton("Rectangle");
-		panel.add(tglbtnRectangle);
-
-
-		ZoneDessin	zone_dessin = new ZoneDessin();
-		zone_dessin.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				Point point_depart = new Point(e.getX(),e.getY());
-				
-				Graphics g = zone_dessin.getGraphics();
-				
-				if(tglbtnLigne.isSelected()) {
-					Point point_arrive = new Point(e.getX()+20, e.getY()+20);	//Valeur par defaut
-					if(tglbtnDroite.isSelected()) {
-						point_arrive.setX(e.getX()+20);
-						point_arrive.setY(e.getY()+20);
-					}
-					if(tglbtnGauche.isSelected()) {
-						point_arrive.setX(e.getX()-20);
-						point_arrive.setY(e.getY()+20);
-					}
-					zone_dessin.paint_ligne(g, point_depart, point_arrive);
-				}
-				
-				if(tglbtnRectangle.isSelected()) {
-					Point point_arrive = new Point(e.getX()+10,e.getY()+15);
-					zone_dessin.paint_carre(g, point_depart, point_arrive);
-				}
-			}			
-		});
-		zone_dessin.setBounds(10, 11, 373, 305);
-		frame.getContentPane().add(zone_dessin);
-		frame.setVisible(true);
-
 	}
-	
-	void setText(String message) {
-		textArea.append("\n\r" + message);
-		textField.setText(null);
-	}
-	
-	void connexion(Socket socket, BufferedReader in, PrintWriter out) {
-		this.socket=socket;
-		this.in=in;
-		this.out=out;
-		try {
-			String message_distant = this.in.readLine();
-			setText(message_distant);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	}
+
 }
-
-
--- src/Reseau/Server.java
+++ src/Reseau/Server.java
@@ -1,59 +1,57 @@
 package Reseau;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Server {
+public class Server {   
 
-	public static void main(String[] args) {
-		ServerSocket socketserver;
-		Socket socketduserveur;
-		BufferedReader in;
-		PrintWriter out;
-		int nombre_client=0;
-		int num_socket=2000;
-		while(nombre_client <2) {
-			try {
-				socketserver = new ServerSocket(num_socket);
-				System.out.println("Le serveur ecoute le port "+ socketserver.getLocalPort());
-				socketduserveur = socketserver.accept(); 
-				System.out.println("Un client s'est connecte");
-				/*
-				out = new PrintWriter(socketduserveur.getOutputStream());
-				out.println("Vous etes connecte  !");
-				out.flush();
-				*/
-				num_socket++;
-				nombre_client++;
-			}
-			catch (IOException e) {
-				e.printStackTrace();
+    public static void main(String[] zero) {
+        ServerSocket socketserver  ;
+        Socket socketduserveur ;
+        BufferedReader in ;
+        PrintWriter out;  
+        try {
+            socketserver = new ServerSocket(2009);
+            System.out.println("Le serveur écoute le port "+socketserver.getLocalPort());
+            socketduserveur = socketserver.accept(); 
+                System.out.println("Un client s'est connecté");
+                out = new PrintWriter(socketduserveur.getOutputStream());
+                in = new BufferedReader (new InputStreamReader (socketduserveur.getInputStream()));
+                out.println("Vous êtes connecté  !");
+                out.flush();
+   
+                
+                out.println("Voulezzz vous jouer ?");
+                out.flush();
+                String message_distant = null;
+                message_distant = in.readLine(); 
+                System.out.println(message_distant);    
+                         
+                if (message_distant == "oui") {	
+                    out.println("alors jouons");
+                }
+                else {out.println("nope");}
+                out.flush();
+                socketduserveur.close();
+                socketserver.close();
 
-			}
-		}
-		/*
-			try {
+        }
+        catch (IOException e) {
+
+            
+
+            e.printStackTrace();
+
+        }
+
+    }
 
 
-				socketduserveur.close();
-				socketserver.close();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-
-			}
-		 */
-	}
-
-	public void send(String message) {
-
-	}
-
-	public void receive(String message) {
-
-	}
 }
+
+
